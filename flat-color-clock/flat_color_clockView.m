4288face4c7a3d339566d85a138fa321977e0ea5
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-10-01 19:10:46 +0200

    implementing current time

--- flat-color-clock/flat_color_clockView.m
+++ flat-color-clock/flat_color_clockView.m
@@ -14,7 +14,10 @@
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     
-    if (self) [self setAnimationTimeInterval:0.1];
+    if (self)
+    {
+        [self setAnimationTimeInterval:0.1];
+    }
     
     return self;
 }
@@ -29,22 +32,6 @@
     [super stopAnimation];
 }
 
-- (void) drawRect:(NSRect)rect
-{
-    [super drawRect:rect];
-    
-    NSDate *current_date = [NSDate date];
-    
-    [self drawBackgroundColor:rect forDate:current_date];
-}
-
-- (void) animateOneFrame
-{
-    [self setNeedsDisplay:true]; // redraw each time
-
-    return;
-}
-
 - (BOOL) hasConfigureSheet
 {
     return false;
@@ -53,6 +40,62 @@
 - (NSWindow*) configureSheet
 {
     return nil;
+}
+
+- (void) animateOneFrame
+{
+    [self setNeedsDisplay:true]; // call drawRect and stuff each frame
+    
+    return;
+}
+
+- (void) drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+    
+    NSDate *current_date = [NSDate date];
+    
+    [self drawBackgroundColor:rect forDate:current_date];
+    
+    [self drawCurrentTime:rect forDate:current_date];
+
+}
+
+- (void) drawCurrentTime: (NSRect) rect forDate: (NSDate*) date
+{
+    NSDateFormatter *time_format = [[NSDateFormatter alloc] init]; [time_format setDateFormat:@"HH : mm : ss"];
+    
+    NSString *time = [time_format stringFromDate:date];
+    
+    NSMutableDictionary *string_attributes = [[NSMutableDictionary alloc] init];
+    
+    [string_attributes setValue:[NSColor whiteColor] forKey:NSForegroundColorAttributeName];
+	
+    [string_attributes setValue:[NSFont fontWithName:@"Verdana" size:30] forKey:NSFontAttributeName];
+    
+    // Do we really need shadows along the way?
+    
+    // NSShadow *string_shadow = [[NSShadow alloc] init];
+    
+    // [string_shadow setShadowColor:[NSColor blackColor]];
+    
+    // NSSize string_shadow_size; string_shadow_size.width = 2; string_shadow_size.height = -2;
+    
+    // [string_shadow setShadowOffset:string_shadow_size];
+    
+    // [string_shadow setShadowBlurRadius:6];
+	
+    // [string_attributes setValue:string_shadow forKey:NSShadowAttributeName];
+    
+    NSSize string_size = [time sizeWithAttributes:string_attributes];
+    
+	NSPoint center_point;
+    
+    center_point.x = (rect.size.width / 2) - (string_size.width / 2);
+	
+    center_point.y = (rect.size.height / 2) - (string_size.height / 2);
+	
+    [time drawAtPoint:center_point withAttributes:string_attributes];
 }
 
 - (void) drawBackgroundColor:(NSRect) rect forDate: (NSDate*) date
