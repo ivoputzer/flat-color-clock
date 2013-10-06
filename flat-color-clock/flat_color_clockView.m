fb6ef930e575c0a67922bc6f2304583a89189062
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-10-06 13:47:45 +0200

    adjusting some stuff

--- flat-color-clock/flat_color_clockView.m
+++ flat-color-clock/flat_color_clockView.m
@@ -10,85 +10,69 @@
 
 @implementation flat_color_clockView
 
-const float _animation_fps = 60.0f;                   // [25.0f] animation frames per second
+const float _animation_fps = 25.0f;                   // [25.0f] animation frames per second
 
 const float _animation_vel = 1 / _animation_fps;      // [0.04f] animation velocity
 
       float _animation_cur = 0;                       // [0.00f] generic frame counter
 
-NSColor *_color_prior;                                // [NSColor blackColor]
-
-NSColor *_color_after;
+NSColor *_color_prior, *_color_after;
 
 NSDate *_current_date;
 
 
+// screensaver initialisation methods
 
 - (id) initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     
-    // todo : load font from bundle instead of using the system one
-    
     if (self)
     {
         [self setAnimationTimeInterval:_animation_vel];
-
-        _current_date = [NSDate date];
         
         _color_prior = [NSColor blackColor];
         
-        _color_after = [self getColorForCurrentDate];
+        _color_after = [self getColorForCurrentDate: _current_date = [NSDate date]];
     }
     
     return self;
 }
 
 
-// screensaver animation callback
+// screensaver animation callbacks
 
 - (void) animateOneFrame
 {
     [self setNeedsDisplay:true]; // forces redraw for each animation-iteration
     
     [self prepareDrawOperation];
-    
-    return;
 }
 
 
 // custom utility methods
 
-- (void) prepareDrawOperation
-{
-    if ( _animation_cur * _animation_vel != 1.0f )
-    {
-        // transitioning from one second to another
-        
-        _animation_cur = _animation_cur + 1; return;
-    }
-    
-    // transition is over, reload date reference and reset counter
-    
-    _current_date = [NSDate date];
-
-    _color_prior = _color_after; // exchanging values
-    
-    _color_after = [self getColorForCurrentDate];
-        
-    _animation_cur = 0;
-}
-
-- (NSColor*) getColorForCurrentDate
-{
-    NSDateComponents *components = [[NSCalendar currentCalendar] components:(NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit) fromDate:_current_date];
-    
-    return [NSColor colorWithCalibratedRed: (float)[components hour] / 23.0f green: (float)[components minute] / 59.0f blue: (float)[components second] / 59.0f alpha:1.0];
-}
+- (BOOL) isTransitioning { return 1.0 != (_animation_cur * _animation_vel); }
 
 - (float) width { return [self bounds].size.width; }
 
 - (float) height { return [self bounds].size.height; }
+
+- (void) prepareDrawOperation
+{
+    if ( [self isTransitioning] ){ _animation_cur = _animation_cur + 1; return; }
+    
+    _color_prior = _color_after; _color_after = [self getColorForCurrentDate: _current_date = [NSDate date]]; // swap color values
+        
+    _animation_cur = 0;
+}
+
+- (NSColor*) getColorForCurrentDate:(NSDate*) date
+{
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:(NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit) fromDate:date];
+    
+    return [NSColor colorWithCalibratedRed:(float)[components hour] / 23.0f green:(float)[components minute] / 59.0f blue:(float)[components second] / 59.0f alpha:1.0];
+}
 
 
 // standard and custom drawing methods
@@ -97,11 +81,7 @@ NSDate *_current_date;
 {
     [super drawRect:rect];
     
-    [self drawBackground];
-    
-    [self drawTimeLabel];
-
-    [self drawColorLabel];
+    [self drawBackground]; [self drawTimeLabel]; [self drawColorLabel];
 }
 
 - (void) drawBackground
@@ -113,9 +93,15 @@ NSDate *_current_date;
 
 - (void) drawTimeLabel
 {
-    NSDateFormatter *string_format = [[NSDateFormatter alloc] init]; [string_format setDateFormat:@"HH· mm· ss"];
+    // date formatting
+    
+    NSDateFormatter *string_format = [[NSDateFormatter alloc] init];
+    
+    [string_format setDateFormat:@"HH· mm· ss"];
     
     NSString *string_time = [string_format stringFromDate:_current_date];
+    
+    // string attributes
     
     NSMutableDictionary *string_attributes = [[NSMutableDictionary alloc] init];
     
@@ -125,17 +111,15 @@ NSDate *_current_date;
     
     NSSize string_size = [string_time sizeWithAttributes:string_attributes];
     
-	NSPoint string_point = NSMakePoint([self width] / 2 - string_size.width / 2, [self height] / 2 - string_size.height / 2);
-	
-    [string_time drawAtPoint:string_point withAttributes:string_attributes];
+    // string drawing
+    
+    [string_time drawAtPoint:NSMakePoint([self width] / 2 - string_size.width / 2, [self height] / 2 - string_size.height / 2) withAttributes:string_attributes];
 }
 
 - (void) drawColorLabel
 {
     NSString *string_rgb = [NSString stringWithFormat:@"R : %i G : %i B : %i",
-                            (int)(255*[_color_after redComponent]),
-                            (int)(255*[_color_after greenComponent]),
-                            (int)(255*[_color_after blueComponent])];
+                            (int)(255 * [_color_after redComponent]), (int)(255 * [_color_after greenComponent]), (int)(255 * [_color_after blueComponent])];
     
     NSMutableDictionary *string_attributes = [[NSMutableDictionary alloc] init];
     
@@ -145,9 +129,7 @@ NSDate *_current_date;
     
     NSSize string_size = [string_rgb sizeWithAttributes:string_attributes];
     
-	NSPoint string_point = NSMakePoint([self width] - (string_size.width + 10), 10);
-	
-    [string_rgb drawAtPoint:string_point withAttributes:string_attributes];
+    [string_rgb drawAtPoint:NSMakePoint([self width] - (string_size.width + 10), 10) withAttributes:string_attributes];
 }
 
 // unused standard callback-methods
